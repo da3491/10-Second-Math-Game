fca60a7bffeb7e761e3d2857147add100c27f495
da3491 <87324456+da3491@users.noreply.github.com>
2022-06-14 19:01:00 -0400

    Additional Features

--- index.js
+++ index.js
@@ -8,33 +8,54 @@
 // 8. The current score is shown.
 // 9. A way to restart the game if time runs out.
 
+// **Extra Features**
+// 1. Multiple question types (+, -, *, /).
+//  a. Make sure (-) questions always have positive whole number answers.
+//  b. Make sure (/) questions always have positive whole number answers.
+// 2. Number limit for the user to set.
+// 3. High score indicator for the high score in the current session.
+
 $(document).ready(function () {
   var playerScore = 0;
   var seconds = 10;
   var timer = null;
+  var highScore = 0;
 
   var updateDom = function () {
     $("#score").text(playerScore);
     $("#timer").text(seconds);
+    $("#high-score").text(highScore);
+    $(".slider-value").text($(".slider").val());
   };
   updateDom();
 
   var createEquation = function () {
-    var a = Math.floor(Math.random() * 10 + 1);
-    var b = Math.floor(Math.random() * 10 + 1);
-    $("#question").text(a + " " + " + " + " " + b);
-    return a + b;
+    var operand = ["+", "-", "*", "/"];
+    randomOp = operand[Math.floor(Math.random() * 3)];
+    var a = Math.floor(Math.random() * $("input.slider").val() + 1);
+    var b = Math.floor(Math.random() * $("input.slider").val() + 1);
+    if (randomOp == "-" || randomOp == "/") {
+      if (b > a) {
+        $("#question").text(b + " " + randomOp + " " + a);
+        return eval(b + randomOp + a);
+      }
+    } else {
+      $("#question").text(a + " " + randomOp + " " + b);
+      return eval(a + randomOp + b);
+    }
   };
-  var solution = createEquation();
 
   var startTimer = function () {
     if (!timer) {
       timer = setInterval(function () {
         seconds--;
         updateDom();
-        if (seconds == 0) {
+        if (seconds <= 0) {
           clearInterval(timer);
           timer = null;
+          highscore =
+            playerScore > highScore ? (highScore = playerScore) : playerScore;
+          $(".guess").prop("disabled", true);
           $("#play-btn").removeClass("hidden");
         }
       }, 1000);
@@ -55,10 +76,18 @@ $(document).ready(function () {
 
   // Timer starts when input changes or button is clicked
   $("#user-input").on("click change", ".guess", function () {
-    startTimer();
+    if (seconds > 0) {
+      startTimer();
+      solution = createEquation();
+    }
   });
   // Processes players guess on click
-  $("button.guess").on("click", function () {
+  $("button.guess").on("click submit", function (e) {
+    e.preventDefault();
     processAnswer($("input.guess").val());
+  });
+
+  $(".slider").on("input", function () {
+    updateDom();
   });
 });
